aa4f9e04f751337a01521aee774ad5a1ac3fb944
cls <731058610@qq.com>
2017-03-02 10:01:28 +0800

    更改直播间的封面为头像

--- app/src/main/java/cn/ucai/live/ui/activity/LiveListFragment.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveListFragment.java
@@ -30,6 +30,7 @@ import com.hyphenate.EMChatRoomChangeListener;
 import com.hyphenate.chat.EMChatRoom;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMCursorResult;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.exceptions.HyphenateException;
 
 import cn.ucai.live.R;
@@ -256,7 +257,7 @@ public class LiveListFragment extends Fragment {
             liveRoom.setAudienceNum(room.getAffiliationsCount());
             liveRoom.setId(room.getId());
             liveRoom.setChatroomId(room.getId());
-            liveRoom.setCover(R.drawable.test1);
+            liveRoom.setCover(EaseUserUtils.getAppUserInfo(room.getOwner()).getAvatar());
             liveRoom.setAnchorId(room.getOwner());
             roomList.add(liveRoom);
         }
